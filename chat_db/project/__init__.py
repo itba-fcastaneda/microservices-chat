24c692a93171fb022836c72fc00da5ed8bc2cf4f
Federico Castañeda <fcastaneda@allurion.com>
2023-03-20 08:41:01 -0300

    Upgrade chat_db

--- chat_db/project/__init__.py
+++ chat_db/project/__init__.py
@@ -1,12 +1,6 @@
 from pyms.flask.app import Microservice
 from project.models.init_db import db
 
-
-__author__ = "Alberto Vara"
-__email__ = "a.vara.1986@gmail.com"
-__version__ = "0.1.0"
-
-
 class MyMicroservice(Microservice):
     def init_libs(self):
         db.init_app(self.application)
